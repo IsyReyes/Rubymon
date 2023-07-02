cb6f1cdab8a12652a226ad5130f3e1d0f5cf3197
IsyReyes <isyreyes@hotmail.es>
2023-07-02 00:48:26 -0600

    saved game menu no longer crashes if no saved files are found, stats load every time

--- game.rb
+++ game.rb
@@ -86,7 +86,7 @@ def new_game
   puts "When you feel ready, you can challenge BROCK, the PEWTER's GYM LEADER"
   puts "-----------------------------------------------------------"
   @game_state = Saving.new_game_save(@player_name, @player_main_pokemon, @pokemon_nickname)
-  # @game_state
+  @game_state
   selection_menu
 end
 
@@ -100,6 +100,7 @@ def selection_menu
   case gameplay_menu_choice
   when 'Stats'
     Stats.print_stats(@game_state)
+    selection_menu
     when 'Train'
     #call method for initiating a random battle
     #hashes as we have them rn won't work, change save method to add lvl key and value
